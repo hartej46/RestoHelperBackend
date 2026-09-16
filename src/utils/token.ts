285904ddd72bff2da262f695a9da78edfe2c84d6
hartej46 <hartejsinghsandhu1977@gmail.com>
2026-09-16 16:30:40 +0530

    changed file name and changed expirydate type to date

---
+++ src/utils/token.ts
@@ -0,0 +1,81 @@
+import jwt from "jsonwebtoken";
+import AppError from "./error";
+import crypto from "node:crypto";
+
+const generateAccessToken = (userId : string, sessionId : string, email: string, name: string) => {
+    if ( !process.env.ACCESS_TOKEN_GENERATOR || !process.env.ACCESS_TOKEN_EXPIRY) {
+        throw new AppError("No env file found", 500)
+    }
+    return jwt.sign({
+            userId: userId,
+            email: email,
+            name: name,
+            sessionId : sessionId
+        },
+        process.env.ACCESS_TOKEN_GENERATOR,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY as jwt.SignOptions["expiresIn"]
+        }
+    )
+};
+
+const generateRefreshToken = (userId : string, sessionId : string, email: string, name: string) => {
+    if ( !process.env.REFRESH_TOKEN_GENERATOR || !process.env.REFRESH_TOKEN_EXPIRY) {
+        throw new AppError("No env file found", 500)
+    }
+    return jwt.sign({
+            user_id: userId,
+            email: email,
+            name: name,
+            sessionId: sessionId
+        },
+        process.env.REFRESH_TOKEN_GENERATOR,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY as jwt.SignOptions["expiresIn"]
+        }
+    )
+};
+
+const verifyAccessToken = (token : string) => {
+    const res = jwt.verify(token, process.env.ACCESS_TOKEN_GENERATOR!);
+    return res;
+};
+
+const verifyRefreshToken = (token : string) => {
+    const res = jwt.verify(token, process.env.REFRESH_TOKEN_GENERATOR!);
+    return res;
+};
+
+const generateCsrfToken = ( token : string, key : string ) => {
+    const csrfToken = crypto.createHmac("sha256", key)
+        .update( token )
+        .digest("hex");
+
+        return csrfToken
+}
+
+const isCsrfTokenValid = ( csrfHeader : string, csrfCookie : string, csrf_secret : string , token : string ) => {
+    const expectedCsrfToken = crypto
+            .createHmac("sha256", csrf_secret)
+            .update( token )
+            .digest("hex");
+    
+        const expectedCsrfBuffer = Buffer.from(expectedCsrfToken);
+        const headerCsrfBuffer = Buffer.from(csrfHeader);
+        const cookieCsrfBuffer = Buffer.from(csrfCookie);
+    
+        const isTokenSame = expectedCsrfBuffer == headerCsrfBuffer && expectedCsrfBuffer == cookieCsrfBuffer;
+    
+        const isTokenTimingSame = crypto.timingSafeEqual( expectedCsrfBuffer, headerCsrfBuffer) && crypto.timingSafeEqual( expectedCsrfBuffer, cookieCsrfBuffer );
+
+        return isTokenSame && isTokenTimingSame;
+}
+
+export {
+    generateAccessToken,
+    generateRefreshToken,
+    verifyAccessToken,
+    verifyRefreshToken,
+    generateCsrfToken,
+    isCsrfTokenValid
+};
