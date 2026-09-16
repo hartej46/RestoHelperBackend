14cbe372120a9e1f5651080d1ecef0a351ef0e9a
hartej46 <hartejsinghsandhu1977@gmail.com>
2026-09-16 16:23:44 +0530

    update cookie options

--- src/constanst.ts
+++ src/constanst.ts
@@ -1,1 +1,14 @@
-const DB_NAME = "Restro_helper"
+import { CookieOptions } from "express";
+export const DB_NAME = "Restro_helper";
+
+export const refreshTokenOption : CookieOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax"
+};
+
+export const csrfTokenOptions :  CookieOptions = {
+    httpOnly: false,
+    secure: true,
+    sameSite: "lax"
+};
